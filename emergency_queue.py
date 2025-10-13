e8ac6d45d24442909ab66f346871b2e733aaaf79
trentonbates <batestrenton@outlook.com>
2025-10-13 02:25:47 -0500

    updated assignment

--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,28 @@
 class Patient:
-    pass # Delete this line and implement the Patient class
-
-
+    def __init__(self, name, urgency):
+        self.name = name
+        self.urgency = urgency
 
 class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+    def __init__(self):
+        self.data = list()
 
+    def heapify_up(self, index):
+        ...
 
+    def heapify_down(self, index):
+        ...
 
+    def insert(self, patient):
+        ...
+
+    def print_heap(self):
+        ...
+
+    def peek(self):
+        ...
+
+    def remove_min(self):
+        ...
 
 # Test your MinHeap class here including edge cases
