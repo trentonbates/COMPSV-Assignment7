ed47ab1c747c0993539c55899d7f93085422270a
trentonbates <batestrenton@outlook.com>
2025-10-13 17:03:19 -0500

    finished emergency_queue

--- emergency_queue.py
+++ emergency_queue.py
@@ -8,21 +8,76 @@ class MinHeap:
         self.data = list()
 
     def heapify_up(self, index):
-        ...
+        while index > 0:
+            parent_index = (index - 1) // 2
+            current_patient = self.data[index]
+            parent_patient = self.data[parent_index]
+
+            if current_patient.urgency < parent_patient.urgency:
+                temp = self.data[index]
+                self.data[index] = self.data[parent_index]
+                self.data[parent_index] = temp
+
+                index = parent_index
+            else:
+                break
 
     def heapify_down(self, index):
-        ...
+        left = 2 * index + 1
+        right = 2 * index + 2
+        smallest = index
+
+        if left < len(self.data) and self.data[left].urgency < self.data[smallest].urgency:
+            smallest = left
+
+        if right < len(self.data) and self.data[right].urgency < self.data[smallest].urgency:
+            smallest = right
+        
+        if smallest != index:
+            temp = self.data[index]
+            self.data[index] = self.data[smallest]
+            self.data[smallest] = temp
+
+            self.heapify_down(smallest)
 
     def insert(self, patient):
-        ...
+        self.data.append(patient)
+        self.heapify_up(len(self.data) - 1)
 
     def print_heap(self):
-        ...
+        print("\nCurrent Queue:")
+        for patient in self.data:
+            print(f' - {patient.name} ({patient.urgency})')
+        print()
 
     def peek(self):
-        ...
+        return self.data[0]
 
     def remove_min(self):
-        ...
+        if not self.data:
+            return None
+        
+        if len(self.data) == 1:
+            return self.data.pop()
+        
+        min_value = self.data[0]
+        self.data[0] = self.data.pop()
+        self.heapify_down(0)
+        return min_value
 
-# Test your MinHeap class here including edge cases
+p1 = Patient("Riley", 2)
+print(p1.name)
+print(p1.urgency)
+
+heap = MinHeap()
+heap.insert(Patient("Jordan", 3))
+heap.insert(Patient("Taylor", 1))
+heap.insert(Patient("Avery", 5))
+heap.print_heap()
+
+next_up = heap.peek()
+print(next_up.name, next_up.urgency)
+
+served = heap.remove_min()
+print(served.name)
+heap.print_heap()
